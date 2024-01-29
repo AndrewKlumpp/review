a26b64773ac452d2c27edf4a3694107c4d978250
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-29 09:46:27 -0700

    initial commit

---
+++ removeLastVowel.js
@@ -0,0 +1,9 @@
+function removeLastVowel(word) {
+
+};
+
+
+console.log(removeLastVowel('bootcamp')); // 'bootcmp'
+console.log(removeLastVowel('better')); // 'bettr'
+console.log(removeLastVowel('graph')); // 'grph'
+console.log(removeLastVowel('thy')); // 'thy'
