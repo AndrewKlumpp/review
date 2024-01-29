214eadd9825a7d5d0344fcfffccc53620791906d
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-29 08:30:13 -0700

    finished sumArray

--- sumArray.js
+++ sumArray.js
@@ -1,5 +1,10 @@
 function sumArray(array) {
-  // your code here...
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    let num = array[i];
+    sum += num;
+  }
+  return sum;
 }
 
 console.log(sumArray([5, 6, 4])); // => 15
