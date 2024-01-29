65301433ede167a91202b39d368bb1aae9801f29
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-29 09:57:09 -0700

    initial commit

---
+++ leastCommonMultiple.js
@@ -0,0 +1,10 @@
+function leastCommonMultiple(num1, num2) {
+
+};
+
+
+
+
+console.log(leastCommonMultiple(4, 6)); // 12
+console.log(leastCommonMultiple(3, 5)); // 15
+console.log(leastCommonMultiple(2, 10)); // 10
