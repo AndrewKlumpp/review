582efe7b5ae240b29f988124affee9216860dc51
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-29 08:13:48 -0700

    initial commit

---
+++ countVowels.js
@@ -0,0 +1,7 @@
+function countVowels(word) {
+  // your code here...
+};
+
+console.log(countVowels("bootcamp")); // => 3
+console.log(countVowels("apple")); // => 2
+console.log(countVowels("pizza")); // => 2
