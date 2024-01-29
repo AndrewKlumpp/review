2a911d1b805e093f99c89ad9f09f114a3431d538
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-29 09:32:46 -0700

    finished fizzBuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,5 +1,13 @@
 function fizzBuzz(max) {
-
+  let output = [];
+  for (let i = 0; i < max; i++) {
+    if (i % 3 === 0 && i % 5 !== 0) {
+      output.push(i);
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+      output.push(i);
+    }
+  }
+  return output;
 };
 
 
