151b22e690c879c27b3ba31bdc774860e509e9ef
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-29 08:49:12 -0700

    finished pigLatinWord

---
+++ pigLatinWord.js
@@ -0,0 +1,23 @@
+// So the two rules for our version of Pig Latin are:
+
+// 1. For words that start with a vowel, add 'yay' to the end of the word.
+// 2. For words that start with a non-vowel, move all letters that come
+// **before the first vowel** to the **end of the word** then add 'ay'
+
+function pigLatinWord(word) {
+  let vowels = 'aeiou';
+  for (let i = 0; i < word.length; i++) {
+    let firstLetter = word[0];
+    let letter = word[i];
+    if (vowels.includes(firstLetter)) {
+      return word + "yay";
+    } else if (vowels.includes(letter)) {
+      return word.slice(i) + word.slice(0, i) + "ay";
+    }
+  }
+};
+
+console.log(pigLatinWord("apple")); //=> "appleyay"
+console.log(pigLatinWord("eat")); //=> "eatyay"
+console.log(pigLatinWord("banana")); //=> "ananabay"
+console.log(pigLatinWord("trash")); //=> "ashtray"
