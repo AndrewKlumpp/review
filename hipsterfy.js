55ed7fa459f7ba332a6e3c24acdec7621ef6c130
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-30 10:35:57 -0700

    finished hipsterfy

--- hipsterfy.js
+++ hipsterfy.js
@@ -1,7 +1,25 @@
 function hipsterfy(sentence) {
+  let words = sentence.split(' ');
+  let hipstered = [];
 
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    hipstered.push(removeLastVowel(word));
+  }
+  return hipstered.join(' ');
 };
 
+function removeLastVowel(word) {
+
+  let vowels = 'aeiou';
+  for (let i = word.length - 1; i >= 0; i--) {
+    let letter = word[i];
+    if (vowels.includes(letter)) {
+      return word.slice(0, i) + word.slice(i + 1);
+    }
+  }
+  return word;
+};
 
 console.log(hipsterfy('When should everyone wake up?')); // 'Whn shold everyon wak p?'
 console.log(hipsterfy('get ready for our bootcamp')); // 'gt redy fr or bootcmp'
