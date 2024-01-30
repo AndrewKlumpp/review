136a5fa23727f2a768aee25599107d621b5e49a8
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-30 09:53:01 -0700

    initial commit

---
+++ uncompress.js
@@ -0,0 +1,10 @@
+function uncompress(str) {
+
+};
+
+
+
+
+console.log(uncompress('x3y4z2')); // 'xxxyyyyzz'
+console.log(uncompress('a5b2c4z1')); // 'aaaaabbccccz'
+console.log(uncompress('b1o2t1')); // 'boot'
